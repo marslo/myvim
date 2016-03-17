9bac40a49a6f9bf17f432dc2144ae6a73dea6b93
Marslo <marslo.jiao@gmail.com>
2016-03-17 13:17:39 -0700

    update updatevim.sh for create gvim and vim for windows 64-bit

--- Scripts/updatevim.sh
+++ Scripts/updatevim.sh
@@ -1,16 +1,17 @@
-#i =============================================================================
+# =============================================================================
 #   FileName: updatevim.sh
 #       Desc: update gvim.exe and vim.exe
 #     Author: Marslo
 #      Email: marslo.jiao@gmail.com
 #    Created: 2015-09-17 18:20:27
 #    Version: 0.0.1
-# LastChange: 2015-11-23 20:22:40
+# LastChange: 2016-03-17 13:09:42
 #    History:
 #             0.0.1 | Marslo | init
 # =============================================================================
 
 sour=$HOME/../../Marslo/Tools/Git/vim/src
+dsk=$HOME/Desktop/vim
 
 if [ ! -d "${sour}" ];
 then
@@ -24,9 +25,11 @@ make clean
 git checkout -- *
 
 
+echo '-----------------------------------'
 origver=`git status | grep v7 | awk -F'7.4.' '{print $2}'`
 curver=$(( ${origver}+1 ))
-latestver=`git tag | grep 'v7.4\.' | tail -1 | awk -F'v7.4.' '{print $2}'`
+git tag --sort=v:refname | grep 'v7.4\.' | tail -30
+latestver=`git tag --sort=v:refname | grep 'v7.4\.' | tail -1 | awk -F'v7.4.' '{print $2}'`
 
 if [ 0 -eq ${#origver} ]
 then
@@ -34,9 +37,10 @@ then
 fi
 
 git checkout master
-# git pull
+git pull
 
-latestver=`git tag | grep 'v7.4\.' | tail -1 | awk -F'v7.4.' '{print $2}'`
+echo '-----------------------------------'
+latestver=`git tag --sort=v:refname | grep 'v7.4\.' | tail -1 | awk -F'v7.4.' '{print $2}'`
 
 if [ ${origver} -eq ${latestver} ]
 then
@@ -51,12 +55,12 @@ echo ${latestver}
 for i in `seq -w ${curver} ${latestver}`
 do
   ver=7.4.${i}
-  targ=/cygdrive/c/Users/Marslo_Jiao/Desktop/vim/${ver}
+  targ=${dsk}/${ver}
   echo "----------------------------------- ${ver} --------------------------------------"
 
   git checkout tags/v${ver}
-  make -B -f Make_cyg.mak PYTHON=/cygdrive/c/Marslo/MyProgramFiles/Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 PYTHON3=/cygdrive/c/Marslo/MyProgramFiles/Python35 DYNAMIC_PYTHON3=yes PYTHON3_VER=35 FEATURES=huge IME=yes GIME=yes MBYTE=yes CSCOPE=yes USERNAME=Marslo.Jiao USERDOMAIN=China GUI=yes >log_gui_${ver}.log 2>&1
-  make -B -f Make_cyg.mak PYTHON=/cygdrive/c/Marslo/MyProgramFiles/Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 PYTHON3=/cygdrive/c/Marslo/MyProgramFiles/Python35 DYNAMIC_PYTHON3=yes PYTHON3_VER=35 FEATURES=huge IME=yes GIME=yes MBYTE=yes CSCOPE=yes USERNAME=Marslo.Jiao USERDOMAIN=China GUI=no > log_nogui_${ver}.log 2>&1
+  make -B -f Make_cyg.mak Make_cyg.mak CROSS_COMPILE=x86_64-w64-mingw32- ARCH=x86-64 PYTHON=/cygdrive/c/Marslo/MyProgramFiles/Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 PYTHON3=/cygdrive/c/Marslo/MyProgramFiles/Python35 DYNAMIC_PYTHON3=yes PYTHON3_VER=35 FEATURES=huge IME=yes GIME=yes MBYTE=yes CSCOPE=yes USERNAME=Marslo.Jiao USERDOMAIN=China GUI=yes >log_gui_${ver}.log 2>&1
+  make -B -f Make_cyg.mak Make_cyg.mak CROSS_COMPILE=x86_64-w64-mingw32- ARCH=x86-64 PYTHON=/cygdrive/c/Marslo/MyProgramFiles/Python27 DYNAMIC_PYTHON=yes PYTHON_VER=27 PYTHON3=/cygdrive/c/Marslo/MyProgramFiles/Python35 DYNAMIC_PYTHON3=yes PYTHON3_VER=35 FEATURES=huge IME=yes GIME=yes MBYTE=yes CSCOPE=yes USERNAME=Marslo.Jiao USERDOMAIN=China GUI=no > log_nogui_${ver}.log 2>&1
 
   mkdir -p ${targ}
   mv ${sour}/vim.exe ${sour}/gvim.exe ${targ}
